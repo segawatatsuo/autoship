3fc83bbb140f0570371753016715579a1a458f9e
segawa <segawa@MacBook.local>
2023-01-30 17:45:26 +0900

    1-30 17:45

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,9 +13,9 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'result',
-        'thanks',
-        'ng'
+        'moka/result',
+        'moka/thanks',
+        'moka/ng'
 
     ];
 }
